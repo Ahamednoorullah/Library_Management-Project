3439f5f204fc7f262fe0d36d6c2872606de717ee
ahamednoorullah <ahamednoorullah@gmail.com>
2024-01-08 18:01:55 +0530

    project registration

--- src/main/java/com/chainsys/library/controller/UserController.java
+++ src/main/java/com/chainsys/library/controller/UserController.java
@@ -1,20 +1,23 @@
 package com.chainsys.library.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.chainsys.library.dao.UserRegisterDAO;
+import com.chainsys.library.model.UserRegister;
 
 @Controller
 public class UserController {
 
-//	@Autowired
-//	JdbcTemplate jdbcTemplate;
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 	
-//	@Autowired
-//	UserRegisterDAO userRegisterdao;
+	@Autowired
+	UserRegisterDAO userRegisterdao;
 	
 	@RequestMapping("/")
 	public String home() {
@@ -22,8 +25,17 @@ public class UserController {
 	}
 	
 	@GetMapping("/addUser")
-	public String addUser() {
+	public String addUser(@RequestParam("username") String userName,@RequestParam("email") String email,
+			@RequestParam("password") String password,@RequestParam("cpassword") String confirmPassword) {
+//		UserRegister userRegister = new UserRegister();
+//		userRegister.setUserName(userName);
+//		userRegister.setEmail(email);
+//		userRegister.setPassword(password);
+//		userRegister.setConfirmPassword(confirmPassword);
+//		System.out.println(userRegister.getUserName());
 		System.out.println("in register page");
+		System.out.println(userName);
+		userRegisterdao.saveUser(userName,confirmPassword,email,password);
 		return "register.html";
 	}
 }
--- src/main/java/com/chainsys/library/dao/UserRegisterDAO.java
+++ src/main/java/com/chainsys/library/dao/UserRegisterDAO.java
@@ -1,12 +1,23 @@
 package com.chainsys.library.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.chainsys.library.model.UserRegister;
 
 @Repository
 public class UserRegisterDAO {
 
-	//@Autowired
-	//JdbcTemplate jdbcTemplate;
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	public void saveUser(String userName,String email,String password,String confirmPassword) {
+		String query = "insert into registration(user_name,cnfpassword,email,password) values (?,?,?,?)";
+	    System.out.println(email);
+		Object[] params = {userName,confirmPassword,email,password};
+		//System.out.println(params);
+		jdbcTemplate.update(query, params);
+	}
 	
 }
